3ffc950e53ca22a8f3e3b863b529286c5378cdc9
HuynhKhanh <huynhkhanh23122003@gmail.com>
2026-03-04 13:50:31 +0700

    Add header and home layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import ProductList from "./pages/ProductList/ProductList";
+import Cart from "./pages/Cart/Cart";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/productlist" element={<ProductList />} />
         <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   );
